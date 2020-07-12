17da070442d25d2a207241b8508694073e30db4d
mgbenedicto <miguelangelo.benedicto@gmail.com>
2020-07-12 19:17:21 +0800

    Updates

---
+++ precache-manifest.6128cbc48d47234e3687a904779619e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35c29d2f9887e5301a088afe453eca2",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "63c58569b00474b9c166",
+    "url": "/react-todolist/static/css/main.991d6402.chunk.css"
+  },
+  {
+    "revision": "7f21aa409de6a7a3419c",
+    "url": "/react-todolist/static/js/2.03293708.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todolist/static/js/2.03293708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63c58569b00474b9c166",
+    "url": "/react-todolist/static/js/main.8997c196.chunk.js"
+  },
+  {
+    "revision": "c8e9edcc0b7d98c328a3",
+    "url": "/react-todolist/static/js/runtime-main.5ff039be.js"
+  }
+]);
